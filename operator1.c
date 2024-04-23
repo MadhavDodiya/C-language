a24ce8871fa573fe37670756592dfaf6f18b1fdc
Madhav Dodiya 2017 <105213289+MadhavDodiya@users.noreply.github.com>
2024-04-23 16:51:09 +0530

    Add files via upload

---
+++ operator1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+//	int num1 ;
+//	int num2 ;
+//	
+//	printf("Enter the value of number num1: ");
+//	scanf("%d",&num1);
+//	
+//	printf("Enter the value of number num2: ");
+//	scanf("%d",&num2);
+//	
+//	printf("%d \n",num1+num2);
+//	printf("%d \n",num1-num2);
+//	printf("%d \n",num1*num2);
+//	printf("%d \n",num1/num2);
+
+int a,b;
+//printf("enter the value of a:");
+//scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+
+
+printf("%d",~b);
+
+
+				
+}
