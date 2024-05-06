931474e4e7256fe73abc0c147dd6322ce3eccaac
Madhav Dodiya 2017 <105213289+MadhavDodiya@users.noreply.github.com>
2024-05-06 16:58:45 +0530

    Add files via upload

---
+++ while1.cpp
@@ -0,0 +1,98 @@
+#include<stdio.h>
+
+int main()
+{
+//	int i,j;
+	
+//	for(i=1;i<=5;i++)
+//	{
+//		printf("hello\n");
+//	}
+
+//int i=1,j,c=1;
+//
+//while(i<=5)
+//{
+//	for(j=1;j<=i;j++)
+//	{
+//		printf("%d ",c);
+//		c++;
+//	}
+//	printf("\n");
+//	i++;
+//}
+
+//	int a,b;
+//	
+//	printf("Enter value: ");
+//	scanf("%d",&a);
+//	
+//	printf("Enter value: ");
+//	scanf("%d",&b);
+//	
+//	while(a<=b)
+//	{
+//		
+//		if(a%2==0)
+//		{
+//			("%d\n",a);
+//		}
+//		a++;
+//	}
+	
+//1
+//12
+//123
+//1234
+//	int a=1;
+//	int b;
+//	
+//	while(a<=5)
+//	{
+//		for(b=1;b<=a;b++)
+//		{
+//			printf("%d",b);
+//		}
+//		printf("\n");
+//		a++;
+//	}
+
+//1
+//11
+//111
+//1111
+//11111
+//	int a=1;
+//	int b;
+//	
+//	while(a<=5)
+//	{
+//		for(b=1;b<=a;b++)
+//		{
+//			printf("1",a);
+//		}
+//		printf("\n");
+//		a++;
+//	}
+
+//1
+//22
+//333
+//4444
+//55555
+//	int a=1;
+//	int b;
+//	
+//	while(a<=5)
+//	{
+//		for(b=1;b<=a;b++)
+//		{
+//			printf("%d",a);	
+//		}	
+//		printf("\n");
+//		a++;
+//	}	
+
+
+}
+
